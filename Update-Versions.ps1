71959b03deae2d8a9289e4fc1a54352f2bc23e06
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2023-09-08 19:27:57 +0000

    Chore: Update versions in `Update-Versions.ps1`

--- Update-Versions.ps1
+++ Update-Versions.ps1
@@ -30,8 +30,8 @@ Push-Location $repo
 
 $versionsNew = @(
     '0.4.0'
-    '0.3.1'
-    '0.2.1'
+    '0.3.2'
+    '0.2.2'
     '0.1.0'
 )
 $versionsChanged = Get-VersionsChanged -Versions (Get-DockerImageVariantsVersions) -VersionsNew $versionsNew -AsObject -Descending
